f8159a38000ca547b270dfcf83b859c481b4afb1
Gareth Jones <jones258@gmail.com>
2018-04-28 16:33:46 +1200

    Added `index.js` exporting everything, as a little es6/node experiment.

---
+++ index.js
@@ -0,0 +1,4 @@
+export const BSClientSocket = require('./BSClientSocket');
+export const AbstractBSClient = require('./AbstractBSClient');
+export const EventBSClient = require('./EventBSClient');
+export const ClientMouth = require('./ClientMouth');
