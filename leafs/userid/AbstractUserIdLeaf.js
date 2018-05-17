038f01215383d1bbfceffb92cfc10fd0e846b38c
Gareth Jones <jones258@gmail.com>
2018-05-18 09:50:51 +1200

    Created `AbstractUserIdLeaf`, which makes it easier to implement leafs to manage user id.

---
+++ leafs/userid/AbstractUserIdLeaf.js
@@ -0,0 +1,97 @@
+// region required exceptions
+const AbstractMethodCalledError = require('./../../exceptions/AbstractMethodCalledError');
+// endregion
+
+const Leaf = require('./../Leaf');
+
+/**
+ * Abstract `Leaf` that handles managing & providing a `userId` as and
+ * when required during the usual operations of a BotSocket Client.
+ *
+ * Create a new `Leaf` extending from this class, implement the abstract methods,
+ * and you'll be rubbing elbows with the finest, having crumpets with her highness.
+ *
+ * @extends {Leaf}
+ * @abstract
+ */
+class AbstractUserIdLeaf extends Leaf {
+    // region getters & setters
+    /**
+     * Gets the id of the user that the BotSocket client is currently representing.
+     *
+     * @return {?string}
+     * @abstract
+     */
+    get userId() {
+        throw new AbstractMethodCalledError();
+    }
+
+    /**
+     * Sets the id of the user that the BotSocket client is currently representing.
+     *
+     * @param {?string} userId
+     * @abstract
+     */
+    set userId(userId) {
+        throw new AbstractMethodCalledError();
+    }
+
+    // endregion
+
+    /**
+     * @inheritDoc
+     *
+     * @param {BotSocket.Protocol.Messages.ServerHandshakeData} serverHandshake
+     * @override
+     */
+    processServerHandshake(serverHandshake) {
+        this.userId = serverHandshake.userId;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param {BotSocket.Protocol.Messages.ClientHandshakeData} clientHandshake
+     *
+     * @return {BotSocket.Protocol.Messages.ClientHandshakeData}
+     * @override
+     */
+    supplementClientHandshake(clientHandshake) {
+        const lastResult = arguments[arguments.length - 1];
+
+        return Object.assign(
+            {},
+            clientHandshake,
+            lastResult,
+            { userId: this.userId }
+        );
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param {StaMP.Protocol.QueryMessage} query
+     *
+     * @return {StaMP.Protocol.QueryMessage}
+     * @override
+     */
+    supplementStaMPQuery(query) {
+        const lastResult = arguments[arguments.length - 1];
+
+        return Object.assign(
+            {},
+            query,
+            lastResult,
+            {
+                data: Object.assign(
+                    {},
+                    query.data,
+                    lastResult ? lastResult.data : null,
+                    { senderId: this.userId }
+                )
+            }
+        );
+    }
+}
+
+module.exports = AbstractUserIdLeaf;
