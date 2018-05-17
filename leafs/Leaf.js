d0e6a84a4804223053a1dc2979ec6beaf3902783
Gareth Jones <jones258@gmail.com>
2018-05-18 09:37:32 +1200

    Created a `Leaf` class.
    It's a class that does things.

---
+++ leafs/Leaf.js
@@ -0,0 +1,138 @@
+const moment = require('moment');
+
+/**
+ *
+ */
+class Leaf {
+    constructor() {
+        /**
+         *
+         * @type {?string}
+         * @private
+         */
+        this._userId = null;
+    }
+
+    /**
+     *
+     * @param {boolean} isReconnecting
+     */
+    preConnect(isReconnecting) {
+        // nothing to do
+    }
+
+    /**
+     *
+     */
+    postConnect() {
+        // nothing to do
+    }
+
+    /**
+     * @param {number} disconnectCode
+     */
+    preDisconnect(disconnectCode) {
+        // nothing to do
+    }
+
+    /**
+     *
+     * @param {number} disconnectCode
+     */
+    postDisconnect(disconnectCode) {
+        // nothing to do
+    }
+
+    /**
+     *
+     */
+    errored() {
+        // nothing to do
+    }
+
+    /**
+     * Processes the data returned by the BotSocket server as part of it's handshaking.
+     *
+     * @param {BotSocket.Protocol.Messages.ServerHandshakeData} serverHandshake
+     */
+    processServerHandshake(serverHandshake) {
+        this._userId = serverHandshake.userId || serverHandshake.clientId;
+    }
+
+    /**
+     * Supplements the data that's going to be sent as part of a BotSocket `ClientHandshake`.
+     *
+     * The value returned by the method of this method on a `Leaf` called prior to this
+     * one on the bush is passed as the last parameter, and can be accessed like so:
+     *
+     * ```
+     * const lastResult = arguments[arguments.length - 1];
+     * ```
+     *
+     * @param {BotSocket.Protocol.Messages.ClientHandshakeData} clientHandshake
+     *
+     * @return {BotSocket.Protocol.Messages.ClientHandshakeData}
+     */
+    supplementClientHandshake(clientHandshake) {
+        return clientHandshake;
+    }
+
+    /**
+     * Supplements a StaMP query message that's going to be sent to the BotSocket server.
+     *
+     * The value returned by the method of this method on a `Leaf` called prior to this
+     * one on the bush is passed as the last parameter, and can be accessed like so:
+     *
+     * ```
+     * const lastResult = arguments[arguments.length - 1];
+     * ```
+     *
+     * @param {StaMP.Protocol.Messages.StandardisedQueryMessage} query
+     *
+     * @return {StaMP.Protocol.Messages.StandardisedQueryMessage}
+     */
+    supplementStaMPQuery(query) {
+        return Object.assign(
+            {},
+            query,
+            {
+                data: Object.assign(
+                    {},
+                    query.data,
+                    { senderId: this._userId }
+                )
+            }
+        );
+    }
+
+    /**
+     * Renders a StaMP Letter - a collection of StaMP messages.
+     *
+     * @param {StaMP.Protocol.Letter} letter
+     */
+    renderLetter(letter) {
+        // nothing to do
+    }
+
+    /**
+     * Renders status update messages.
+     *
+     * @param {string} status
+     * @param {BotSocket.StatusLevel} level
+     */
+    renderStatus(status, level) {
+        // nothing to do
+    }
+
+    /**
+     *
+     * @param {string} format
+     *
+     * @return {string}
+     */
+    timestamp(format = 'hh:mm:ssa') {
+        return moment().format(format);
+    }
+}
+
+module.exports = Leaf;
