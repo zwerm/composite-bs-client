f5c13fdfd69ca4a30113a2a4dc0bc507a0de8ec0
Gareth Jones <jones258@gmail.com>
2018-05-20 13:50:19 +1200

    Added exports to `index.js` for all the leafs and `CompositeBSClient`.

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ export const BSClientSocket = require('./BSClientSocket');
 export const AbstractBSClient = require('./AbstractBSClient');
 export const EventBSClient = require('./EventBSClient');
 export const ClientMouth = require('./ClientMouth');
+export const CompositeBSClient = require('./CompositeBSClient');
+
+// region leafs
+export const TalkingLeaf = require('./leafs/TalkingLeaf');
+export const NotifyingLeaf = require('./leafs/NotifyingLeaf');
+export const CookieUserIdLeaf = require('./leafs/userid/CookieUserIdLeaf');
+export const StaticUserIdLeaf = require('./leafs/userid/StaticUserIdLeaf');
+export const StaticTimezoneLeaf = require('./leafs/timezone/StaticTimezoneLeaf');
+export const BrowserLocationLeaf = require('./leafs/location/BrowserLocationLeaf');
+export const StaticLocationLeaf = require('./leafs/location/StaticLocationLeaf');
+
+export const ScrollToBottomOnLetterLeaf = require('./leafs/ScrollToBottomOnLetterLeaf');
+export const ScrollToTopOnLetterLeaf = require('./leafs/ScrollToTopOnLetterLeaf');
+export const EmitStatusMessageEventsLeaf = require('./leafs/EmitStatusMessageEventsLeaf');
+export const EmitLetterMessageEventsLeaf = require('./leafs/EmitLetterMessageEventsLeaf');
+export const AutoReconnectLeaf = require('./leafs/AutoReconnectLeaf');
+// endregion
