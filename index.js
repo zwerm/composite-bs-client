c52631233c5e0345b51ca5cf61b77c69036dcb0e
Gareth Jones <jones258@gmail.com>
2018-06-12 13:53:23 +1200

    Remove `AbstractBSClient` & `EventBSClient` classes, as they're deprecated in favor of `CompositeBSClient`

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 export const BSClientSocket = require('./BSClientSocket');
-export const AbstractBSClient = require('./AbstractBSClient');
-export const EventBSClient = require('./EventBSClient');
 export const ClientMouth = require('./ClientMouth');
 export const CompositeBSClient = require('./CompositeBSClient');
 
